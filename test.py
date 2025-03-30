922ea548230631dab31a1585096f9c64a55e3ce6
Jerry <atxiesz@gmail.com>
2025-03-30 13:38:23 +0800

    refactor(model): 更新模型路径并移除转录任务参数
    将模型路径从distil-whisper-local更改为distil-small.en，并移除generate方法中的task参数

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import torch
 import librosa
 
 # 加载本地保存的模型和处理器
-model = AutoModelForSpeechSeq2Seq.from_pretrained("./distil-whisper-local")
-processor = AutoProcessor.from_pretrained("./distil-whisper-local")
+model = AutoModelForSpeechSeq2Seq.from_pretrained("./distil-small.en")
+processor = AutoProcessor.from_pretrained("./distil-small.en")
 
 # 加载示例音频文件
 audio_path = "./audio/Such_large_processed.wav"
@@ -17,7 +17,7 @@ input_features = processor(audio_input, sampling_rate=16000, return_tensors="pt"
 with torch.no_grad():
     generation_config = model.generation_config
     generation_config.forced_decoder_ids = None  # 显式移除 forced_decoder_ids
-    predicted_ids = model.generate(input_features, task="transcribe", generation_config=generation_config)
+    predicted_ids = model.generate(input_features, generation_config=generation_config)
 
 # 解码转录结果
 transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
